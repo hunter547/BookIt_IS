401a11554dd89e5ebd438531e2eade5cd3a3e4f5
hunter547 <hunter547@gmail.com>
2017-07-03 22:44:25 -0400

    Update Consumable.java

--- Consumable.java
+++ Consumable.java
@@ -8,17 +8,17 @@ import java.util.ArrayList;
 import oracle.jdbc.pool.OracleDataSource;
 
 public class Consumable {
-    private static ArrayList<Consumable> consumableArray = new ArrayList<>();
+    private static ArrayList<Consumable> conArray = new ArrayList<>();
     private int conID; 
     private static int nextID = 0;
     private String conName; 
     private double conCost; 
     private double conSalePrice;
-    private String conDesc;  
+    private String conDesc; 
     private static Connection dbConn;
     private static Statement commStmt;
-    private static ResultSet dbResults;  
-    
+    private static ResultSet dbResults;
+
     public Consumable(){ 
         this.conID=0; 
         this.conName="";  
@@ -33,6 +33,68 @@ public class Consumable {
         this.conCost=conCost; 
         this.conSalePrice = conSalePrice;
         this.conDesc=conDesc;
+    } 
+    
+    public Consumable(int conID,String conName,double conCost,double conSalePrice,String conDesc){ 
+        this.conID= conID; 
+        this.conName=conName; 
+        this.conCost=conCost; 
+        this.conSalePrice = conSalePrice;
+        this.conDesc=conDesc; 
+        if(conID>nextID){ 
+            nextID = conID;
+        }
+    } 
+    
+    public static void newConFromDatabase(int conID, String conName, double conCost, double conSalePrice, String conDesc){ 
+        conArray.add(new Consumable(conID,conName,conCost,conSalePrice,conDesc));
+    } 
+    
+    public static void newCon(String conName,double conCost,double conSalePrice, String conDesc){ 
+        conArray.add(new Consumable(conName,conCost,conSalePrice,conDesc));
+    }
+    
+    public static void fillConsumableArray() { 
+       String sqlQuery = "";
+       sqlQuery = "SELECT * FROM JAVAUSER.CONSUMABLE";
+       sendDBCommand(sqlQuery);
+
+        try {
+            while (dbResults.next()) {
+
+                Consumable.newConFromDatabase(
+                                    dbResults.getInt(1),
+                                    dbResults.getString(2), 
+                                    dbResults.getDouble(3), 
+                                    dbResults.getDouble(4), 
+                                    dbResults.getString(5));
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } 
+    } 
+    
+    public static void clearConsumableTable() {
+        String sqlQuery = "";
+        sqlQuery = "DELETE FROM JAVAUSER.CONSUMABLE";
+        sendDBCommand(sqlQuery);
+    } 
+    
+    public static void insertAllConsumables() { 
+       for(Consumable c: conArray)
+       {  
+        String sqlQuery = "";
+        sqlQuery += "INSERT INTO JAVAUSER.CONSUMABLE (CON_ID, CON_NAME, "
+                 +  "CON_COST, CON_SALE_PRICE, CON_DESC) VALUES (";
+        sqlQuery += c.getConID() + " , '";
+        sqlQuery += c.getConName()+ "', ";
+        sqlQuery += c.getConCost()+ ", "; 
+        sqlQuery += c.getConSalePrice() + ", '";
+        sqlQuery += c.getConDesc() + "')";
+
+        sendDBCommand(sqlQuery);        
+      } 
     }
 
     public int getConID() {
@@ -73,64 +135,13 @@ public class Consumable {
 
     public void setConDesc(String conDesc) {
         this.conDesc = conDesc;
-    }
-    
-    public static void newConsumable (String conName,double conCost,
-            double conSalePrice,String conDesc)
-    {
-        consumableArray.add(new Consumable (conName, conCost, conSalePrice, conDesc));
-    }
-    
-    public static void newConsumableFromDatabase (String conName,double conCost,
-            double conSalePrice,String conDesc)
-    {
-        consumableArray.add(new Consumable (conName, conCost, conSalePrice, conDesc));
-    }
-    
-    public static void fillConsumableArray() { 
-       String sqlQuery = "";
-       sqlQuery = "SELECT * FROM JAVAUSER.CONSUMABLE";
-       sendDBCommand(sqlQuery);
-
-        try {
-            while (dbResults.next()) {
-
-                        newConsumableFromDatabase(
-                                    dbResults.getString(1),
-                                    dbResults.getDouble(2), 
-                                    dbResults.getDouble(3), 
-                                    dbResults.getString(4));
-
-            }
-        } catch (SQLException e) {
-            System.out.println(e.toString());
-        } 
+    } 
+    public double getConSalePrice() {
+        return conSalePrice;
     } 
     
-    public static void clearConsumableTable() {
-        String sqlQuery = "";
-        sqlQuery = "DELETE FROM JAVAUSER.CONSUMABLE";
-        sendDBCommand(sqlQuery);
-    } 
-    
-    public static void insertAllConsumables() { 
-       for(Consumable c: consumableArray)
-       {  
-        String sqlQuery = "";
-        sqlQuery += "INSERT INTO JAVAUSER.CONSUMABLE (CONSUMABLEID, CONSUMABLENAME,"
-                + "CONSUMABLECOST, CONSUMABLEPRICE, CONSUMABLEDESCRIPTION) VALUES (";
-        sqlQuery += c.getConID() + " , '";
-        sqlQuery += c.getConName() + " , '";
-        sqlQuery += c.getConCost() + "', '";
-        sqlQuery += c.getConPrice() + "', '"; 
-        sqlQuery += c.getConDesc() + "')";
-
-        sendDBCommand(sqlQuery);        
-      } 
-    }
-    
-    public static ArrayList getConsumableArray(){ 
-        return consumableArray;
+    public void setConSalePrice(double conSalePrice){ 
+        this.conSalePrice=conSalePrice;
     }
     
     private static void sendDBCommand(String sqlQuery) {
@@ -149,5 +160,9 @@ public class Consumable {
         } catch (SQLException e) {
             System.out.println(e.toString());
         }
+    } 
+    
+    public static ArrayList<Consumable> getConsumableArray(){ 
+        return conArray;
     }
 }
